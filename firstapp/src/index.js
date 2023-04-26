6960548db8852acde634f0afe4e19be5aaa72bba
aakashdeveloper <ahanda205@gmail.com>
2023-04-26 20:53:44 +0530

    html

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './container/App';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App/>)
+root.render(<App/>);
 
 
 
